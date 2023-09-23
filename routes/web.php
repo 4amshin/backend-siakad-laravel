b934ea3f3c6d7748f646696c968731b71bdbf109
Rely Arfadillah <shinmendenz@gmail.com>
2023-09-23 10:53:46 +0800

    implement auth (login, register)

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return view('pages.dashboard-siakad', ['type_menu' => '']);
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login');
-})->name('login');
+Route::middleware(['auth'])->group(function() {
+    Route::get('home', function() {
+        return view('pages.dashboard-siakad', ['type_menu' => '']);
+    })->name('home');
+});
 
-Route::get('/forgot-password', function () {
-    return view('pages.auth.auth-forgot-password');
-})->name('forgot-password');
+// Route::get('/login', function () {
+//     return view('pages.auth.auth-login');
+// })->name('login');
 
-Route::get('/reset-password', function () {
-    return view('pages.auth.auth-reset-password');
-})->name('reset-password');
+// Route::get('/register', function () {
+//     return view('pages.auth.auth-register');
+// })->name('register');
+
+// Route::get('/forgot', function () {
+//     return view('pages.auth.auth-forgot');
+// })->name('forgot');
+
+// Route::get('/reset-password', function () {
+//     return view('pages.auth.auth-reset-password');
+// })->name('reset-password');
 
 
-Route::get('/register', function () {
-    return view('pages.auth.auth-register');
-})->name('register');
+
